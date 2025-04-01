f0f82a33379a716b472f99114fe47f2c0a258359
ifad <yusuffadhlih123@gmail.com>
2025-04-01 19:55:48 +0700

    finished project

---
+++ src/components/Hero.jsx
@@ -0,0 +1,116 @@
+import React from "react";
+import Marquee from "react-fast-marquee";
+import AnimatedContent from "./AnimatedContent";
+
+const Hero = () => {
+  const skills = [
+    { name: "Express", img: "https://skillicons.dev/icons?i=express" },
+    { name: "React", img: "https://skillicons.dev/icons?i=react" },
+    { name: "MongoDB", img: "https://skillicons.dev/icons?i=mongodb" },
+    { name: "HTML", img: "https://skillicons.dev/icons?i=html" },
+    { name: "CSS", img: "https://skillicons.dev/icons?i=css" },
+    { name: "Tailwind", img: "https://skillicons.dev/icons?i=tailwind" },
+    { name: "NodeJS", img: "https://skillicons.dev/icons?i=nodejs" },
+    { name: "Python", img: "https://skillicons.dev/icons?i=py" },
+    { name: "JavaScript", img: "https://skillicons.dev/icons?i=js" },
+  ];
+
+  return (
+    <div
+      className="text-center h-screen flex flex-col justify-center items-center relative md:px-0 px-[2rem]"
+      id="home"
+    >
+      <AnimatedContent
+        distance={150}
+        direction="horizontal"
+        reverse={false}
+        config={{ tension: 80, friction: 20 }}
+        initialOpacity={0.2}
+        animateOpacity
+        scale={1}
+        threshold={0.2}
+      >
+        <p className="text-[#3CD98E] mt-[9rem] ">Front End Developer</p>
+      </AnimatedContent>
+      <AnimatedContent
+        distance={150}
+        direction="horizontal"
+        reverse={false}
+        config={{ tension: 80, friction: 20 }}
+        initialOpacity={0}
+        animateOpacity
+        scale={1}
+        delay={400}
+        threshold={0.2}
+      >
+        <h1 className="text-6xl font-semibold my-3">Ifad Yusuf</h1>
+      </AnimatedContent>
+      <AnimatedContent
+        distance={150}
+        direction="horizontal"
+        reverse={false}
+        config={{ tension: 80, friction: 20 }}
+        initialOpacity={0}
+        animateOpacity
+        scale={1}
+        delay={800}
+        threshold={0.2}
+      >
+        <p className="md:max-w-[65%] max-w-[90%] text-white opacity-75 mx-auto">
+          Crafting responsive websites with seamless experience, elegant design,
+          and intuitive interactions.
+        </p>
+      </AnimatedContent>
+
+      <AnimatedContent
+        distance={150}
+        direction="horizontal"
+        reverse={false}
+        config={{ tension: 80, friction: 20 }}
+        initialOpacity={0}
+        animateOpacity
+        scale={1}
+        delay={1000}
+        threshold={0.2}
+      >
+        <button className="rounded-full px-[2.5rem] py-2 bg-[#3CD98E] text-black mt-[1.5rem] cursor-pointer text-sm ">
+          Hire Me!
+        </button>
+      </AnimatedContent>
+
+      {/* Marquee Scroll */}
+      <AnimatedContent
+        distance={150}
+        direction="vertical"
+        reverse={false}
+        config={{ tension: 80, friction: 20 }}
+        initialOpacity={0}
+        animateOpacity
+        delay={1100}
+        scale={1}
+        threshold={0.2}
+      >
+        <div className="relative md:max-w-[800px] max-w-[300px] mx-auto md:mt-[6rem] mt-[7rem] overflow-hidden ">
+          {/* Left Fade */}
+          <div className="absolute left-0 top-0 h-full md:w-40 w-5 bg-[linear-gradient(to_right,rgba(10,10,11,1),transparent)] z-10 pointer-events-none" />
+          {/* Right Fade */}
+          <div className="absolute right-0 top-0 h-full md:w-40 w-5 bg-[linear-gradient(to_left,rgba(10,10,11,1),transparent)] z-10 pointer-events-none" />
+
+          <Marquee speed={60} gradient={false} pauseOnHover={false}>
+            {skills.map((skill, index) => (
+              <div
+                key={index}
+                className="flex items-center px-4 py-2 my-2 gap-1 mx-3 text-white bg-[#1B1B1B] outline-1 outline-[#424242] rounded-full"
+              >
+                <img src={skill.img} alt={skill.name} className="w-5 mr-2" />
+                <span className="text-sm">{skill.name}</span>
+              </div>
+            ))}
+          </Marquee>
+        </div>
+      </AnimatedContent>
+    </div>
+  );
+};
+
+export default Hero;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,42 @@
+import { useState } from "react";
+import { Link } from "react-scroll";
+
+const Navbar = () => {
+  const [isActive, setIsActive] = useState("Home");
+  const navItems = [
+    { name: "Home", path: "home" },
+    { name: "About", path: "about" },
+    { name: "Projects", path: "projects" },
+    { name: "Contact", path: "contact" },
+  ];
+
+  return (
+    <div
+      className="fixed top-0 left-0 z-50 px-[2rem]"
+      style={{ width: "100vw" }}
+    >
+      <div className="max-w-[380px] mx-auto  rounded-full bg-[#1B1B1B]/80 backdrop-blur py-[10px] md:mt-[1.5rem] mt-[2rem] outline-1 outline-[#424242] ">
+        <ul className="md:space-x-7 space-x-5 flex items-center justify-center">
+          {navItems.map((item) => (
+            <li key={item.path}>
+              <Link
+                to={item.path}
+                smooth={true}
+                duration={500}
+                onClick={() => {
+                  setIsActive(item.name);
+                  console.log(isActive);
+                }}
+                className="cursor-pointer text-sm hover:text-[#3CD98E]"
+              >
+                {item.name}
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
